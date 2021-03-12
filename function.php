19ab8cf07f2330bc1af1ee0eaf4f56c3dbf01e9a
artluzan <77586145+artluzan@users.noreply.github.com>
2021-03-12 19:17:52 +0200

    Update function.php

--- function.php
+++ function.php
@@ -5,3 +5,10 @@ function smartwp_remove_wp_block_library_css(){
     wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
 } 
 add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
+add_filter('use_block_editor_for_post_type', '__return_false', 10); // Hide editor
+
+add_action('init', function() {
+remove_post_type_support( 'post', 'editor' );
+remove_post_type_support( 'page', 'editor' );
+}, 99);
